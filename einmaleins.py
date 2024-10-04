5e503c4525faaa280e7d9bfe824883bd13ddc627
Andre Mueller <andre@kiwisound.de>
2024-10-04 08:38:03 +0200

    add possibility for making larger starting numbers

--- einmaleins.py
+++ einmaleins.py
@@ -3,9 +3,9 @@ import itertools
 from random import shuffle, randrange
 
 
-def generate_exercises(limit, num_exercises, type_exercises):
+def generate_exercises(min_size, limit, num_exercises, type_exercises):
     exercise_list = []
-    for i, j in itertools.product(range(1, limit + 1), range(1, limit + 1)):
+    for i, j in itertools.product(range(min_size, limit + 1), range(1, 11)):
         exercise_list.append((i, j, (i) * (j), '*'))
     while len(exercise_list) < num_exercises:
         exercise_list += exercise_list
@@ -52,6 +52,7 @@ def get_user_value(question, allowed_values):
 
 
 if __name__ == "__main__":
+    minsize = get_user_input("Ab welcher Zahl soll das 1*1 starten? [1]", 1)
     gridsize = get_user_input("Bis zu welcher Zahl soll das 1*1 gehen? [10]", 10)
     print("OK, dann geht es bis %s*%s." % (gridsize, gridsize))
     num_exercises = get_user_input("Wie viele Aufgaben sollen gestellt werden? [%s]" % (gridsize ** 2),
@@ -60,17 +61,24 @@ if __name__ == "__main__":
     type_exercises = get_user_value("Nur (M)ultiplikation, nur (D)ivision oder (b)eides?", ('m', 'd', 'b'))
     if type_exercises=='b':
         percentage_division = get_user_input("Wieviel Prozent sollen Division sein? [50]", 50)
-    exercise_list = generate_exercises(gridsize, num_exercises, type_exercises)
+    exercise_list = generate_exercises(minsize, gridsize, num_exercises, type_exercises)
     incorrect_answers = 0
     started = datetime.datetime.now().replace(microsecond=0)
-    while len(exercise_list) > 0:
+    do_end = False
+    count = 0
+    while not do_end and len(exercise_list) > 0:
         if len(exercise_list) % 10 == 0 and len(exercise_list) != num_exercises:
             print("Es sind noch %s Aufgaben uebrig." % (len(exercise_list)))
         exercise = exercise_list.pop(0)
         result = ''
         failed = False
-        while True:
+        while not do_end:
             result = input(str(exercise[0]) + ' ' + exercise[3] + ' ' + str(exercise[1]) + " = ")
+            result = result.strip()
+            if result == 'q':
+                do_end = True
+                break
+            count = count + 1
             if result != str(exercise[2]):
                 if not failed:
                     print("Das Ergebnis ist leider nicht richtig. Versuche es noch einmal.")
@@ -82,5 +90,5 @@ if __name__ == "__main__":
                 break
     ended = datetime.datetime.now().replace(microsecond=0)
     print("Du hast von %s Aufgaben %s im ersten Anlauf richtig beantwortet." % (
-        num_exercises, num_exercises - incorrect_answers))
+        count, count - incorrect_answers))
     print("Insgesamt hast Du %s gebraucht." % (ended - started))
